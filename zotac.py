3c3af5d50055a1826e4a657b5fda115c2de97554
TrustyJar <88812001+TrustyJar@users.noreply.github.com>
2022-07-09 23:24:29 -0400

    Update zotac.py

--- zotac.py
+++ zotac.py
@@ -18,7 +18,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from twocaptcha import TwoCaptcha
 
 def solveCaptcha(sitekey):
-    api_key = "cbc83cc83bb9b6187dd71583548dea12"
+    api_key = "<YOURAPIKEY>"
     solver = TwoCaptcha(api_key)
 
     try:
